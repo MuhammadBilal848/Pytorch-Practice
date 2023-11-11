8f135df4b8c6d54ce2ba18e291529c7c5d4d68f8
Bilal Haneef <57318407+MuhammadBilal848@users.noreply.github.com>
2023-11-11 16:27:09 +0500

    commiting smol changes in 2nd nb

--- Gradient_Calc_AudoGrade.py
+++ Gradient_Calc_AudoGrade.py
@@ -17,14 +17,19 @@ import torch
 # z.backward() # backprop of dz w.r.t dx: (dz/dx)
 # print('Gradients of dz/dx: ',x.grad) 
 
-x = torch.tensor([1.,2.,3.],requires_grad=True)
-print(x)
-z = x**2
-print(z)
-z = z.mean()
-print(z)
-z.backward()
-print(x.grad)
+# x = torch.tensor([1.,2.,3.],requires_grad=True)
+# print(x)
+# z = x**2
+# print(z)
+
+# # if z is not a singular value we will get an error, so in that case we have to pass another tensor in backward
+# # function as parameter
+# v = torch.tensor([0.1,0.2,0.3])
+# z.backward(v) # this will be (dz/dx) * v
+# print(x.grad)
 
 
-
+####################### HOW TO STOP PYTORCH TO CREATE GRADIENT HISTORY ######################
+# 1. var_name.require_grad_(False)
+# 2. var_name.detach()
+# 3. with torch.no_grad():
