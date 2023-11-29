6b3bc8b4ee7d147a6b2727de6f5af5e8fda5dcc1
Bilal Haneef <57318407+MuhammadBilal848@users.noreply.github.com>
2023-11-29 16:12:05 +0500

    commiting new changes

--- 2. Gradient_Calc_AudoGrade.py
+++ 2. Gradient_Calc_AudoGrade.py
@@ -1,53 +1,55 @@
 import torch
 
-# # lets try and do gradient calculation
-# x = torch.tensor([1,2,3,4,5],dtype=float,requires_grad = True)
-# x = torch.randn(5,requires_grad = True)
-# print('printing x',x)
+# lets try and do gradient calculation
+x = torch.tensor([1,2,3,4,5],dtype=float,requires_grad = True)
+x = torch.randn(5,requires_grad = True)
+print('printing x',x)
 
-# y = x + 2
-# print('printing y',y)
+y = x + 2
+print('printing y',y)
 
-# z = y*9*x
-# print('printing z' , z)
-# z = z.mean() # we have to compress values to a singular in order to calculate gradient.or else we have to
-# # pass equal length tensor in backword() function as param
-# print('printing z' ,z)
-# # now to calculate gradient(derivative) we can use backpropagation method in torch 
-# z.backward() # backprop of dz w.r.t dx: (dz/dx)
-# print('Gradients of dz/dx: ',x.grad) 
+z = y*9*x
+print('printing z' , z)
+z = z.mean() # we have to compress values to a singular in order to calculate gradient.or else we have to
+# pass equal length tensor in backword() function as param
+print('printing z' ,z)
+# now to calculate gradient(derivative) we can use backpropagation method in torch 
+z.backward() # backprop of dz w.r.t dx: (dz/dx)
+print('Gradients of dz/dx: ',x.grad) 
 
-# x = torch.tensor([1.,2.,3.],requires_grad=True)
-# print(x)
-# z = x**2
-# print(z)
+x = torch.tensor([1.,2.,3.],requires_grad=True)
+print(x)
+z = x**2
+print(z)
 
-# # if z is not a singular value we will get an error, so in that case we have to pass another tensor in backward
-# # function as parameter
-# v = torch.tensor([0.1,0.2,0.3])
-# z.backward(v) # this will be (dz/dx) * v
-# print(x.grad)
-
-
-# ###################### HOW TO STOP PYTORCH TO CREATE GRADIENT HISTORY ######################
-# # 1st way: var_name.require_grad_(False)
-# x1 = torch.randn(3,requires_grad = True)
-# x1.requires_grad_(False)
-# print(x1)
-
-# # 2nd way: var_name.detach()
-
-# x2 = torch.randn(3,requires_grad = True)
-# x2 = x2.detach()
-# print(x2)
-
-# # 3rd way: with torch.no_grad():
+# if z is not a singular value we will get an error, so in that case we have to pass another tensor in backward
+# function as parameter
+v = torch.tensor([0.1,0.2,0.3])
+z.backward(v) # this will be (dz/dx) * v
+print(x.grad)
 
 
-# x3 = torch.randn(3,requires_grad = True)
-# with torch.no_grad():
-#         y = x3+2*4
-#     print(y)
+###################### HOW TO STOP PYTORCH TO CREATE GRADIENT HISTORY ######################
+# 1st way: var_name.require_grad_(False)
+x1 = torch.randn(3,requires_grad = True)
+x1.requires_grad_(False)
+print(x1)
+
+# 2nd way: var_name.detach()
+
+x2 = torch.randn(3,requires_grad = True)
+x2 = x2.detach()
+print(x2)
+
+# 3rd way: with torch.no_grad():
+
+
+x3 = torch.randn(3,requires_grad = True)
+with torch.no_grad():
+    y = x3+2*4
+    print(y)
+
+
 
 weights = torch.ones(5,requires_grad=True)
 print(weights)
@@ -59,3 +61,5 @@ for epoch in range(6):
     print('printing weights',weights.grad)
     print('After printing model_out',model_out)
     weights.grad.zero_() # setting gradienst to zero so that every time it calculates from the start
+
+
