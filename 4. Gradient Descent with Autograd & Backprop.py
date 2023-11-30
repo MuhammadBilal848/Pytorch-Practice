86dad4b5d55db64387701419e2faec7a34506058
Bilal Haneef <57318407+MuhammadBilal848@users.noreply.github.com>
2023-11-30 15:30:11 +0500

    commiting completed 4th file

--- 4. Gradient Descent with Autograd & Backprop.py
+++ 4. Gradient Descent with Autograd & Backprop.py
@@ -1,36 +1,97 @@
-import numpy as np
+# import numpy as np
 
-# here we will implement linear regression from scratch
+# ##########################################################################################################
+# #                      here we will implement linear regression manually from scratch 
+# ##########################################################################################################
 
-# eqn of linear reg , we are ignoring bias here
-# f = w * x
 
-x = np.array([1,2,3,4,5],dtype = np.float32) # input
-y = np.array([3,5,7,9,11],dtype = np.float32) # output
+# # eqn of linear reg , we are ignoring bias here
+# # f = w * x
+
+# x = np.array([1,2,3,4,5],dtype = np.float32) # input
+# y = np.array([3,5,11,7,9],dtype = np.float32) # output
+
+
+# # here we define the initial weight
+# w = 0.0
+
+# # define model
+# def forward(x):
+#     return w * x
+
+# # loss
+# # MSE - (1(w*x - y)**2)/N
+# def loss(y,y_pred):
+#     return ((y_pred-y)**2).mean()
+
+# # gradient
+# # dJ/dw = 1(2x (w*x -y))/ N
+# def gradient(x,y,y_pred):
+#     return np.dot(2*x,y_pred-y).mean()
+
+
+# print(f'Prediction before training: f(5) = {forward(5):.3f}')   
+
+# # training
+# lr = 0.01
+# n_iters = 10
+
+# for epoch in range(n_iters):
+
+#     # predictions
+#     ypred = forward(x)
+
+#     # loss
+#     l = loss(y,ypred)
+
+#     # gradients
+#     dw = gradient(x,y,ypred) # dw/dl
+
+#     # updating weights
+#     w -= lr * dw
+
+#     if epoch % 1 == 0:
+#         print(f'epoch {epoch+1}: w = {w:.3f} , loss = {l:.8f}')
+
+
+# print(f'Prediction after training: f(5) = {forward(2):.3f}')   
+# print(f'Prediction after training: f(5) = {forward(6):.3f}')   
+
+
+##########################################################################################################
+#                      here we will implement linear regression using pytorch 
+##########################################################################################################
+
+
+import torch
+
+x = torch.tensor([1,2,3,4,5],dtype = torch.float32) # input
+y = torch.tensor([3,5,11,7,9],dtype = torch.float32) # output
 
 
 # here we define the initial weight
-w = 0.0
+w = torch.tensor(0.0,requires_grad=True,dtype=torch.float32)
 
 # define model
-def forward(a):
+def forward(x):
     return w * x
 
 # loss
 # MSE - (1(w*x - y)**2)/N
 def loss(y,y_pred):
-    return ((y-y_pred)**2).mean()
+    return ((y_pred-y)**2).mean()
 
 # gradient
 # dJ/dw = 1(2x (w*x -y))/ N
 def gradient(x,y,y_pred):
-    return np.dot(2*x,y-y_pred).mean()
+    return torch.dot(2*x,y_pred-y).mean()
+
 
 print(f'Prediction before training: f(5) = {forward(5):.3f}')   
 
 # training
 lr = 0.01
-n_iters = 10
+n_iters = 100
 
 for epoch in range(n_iters):
 
@@ -41,14 +102,18 @@ for epoch in range(n_iters):
     l = loss(y,ypred)
 
     # gradients
-    dw = gradient(x,y,ypred)
+    l.backward()
 
-    # updating weights
-    w -= lr * dw
+    # updating weights , we donot want to keep track of gradients so we use no_grad
+    with torch.no_grad():
+        w -= lr * w.grad
+
+    # we also have to empty the gradients after every iteration, as torch will write and accumulate gradients
+    w.grad.zero_()
 
     if epoch % 1 == 0:
+        print(f'epoch {epoch+1}: w = {w:.3f} , loss = {l:.8f}')
 
 
-
-
-
+print(f'Prediction after training: f(5) = {forward(2):.3f}')   
+print(f'Prediction after training: f(5) = {forward(6):.3f}')   
