dc42a5975e979b8447f281a56eb6e11292824760
Bilal Haneef <57318407+MuhammadBilal848@users.noreply.github.com>
2023-12-04 17:50:03 +0500

    commiting 9th file

--- 8. Dataset & Dataloader.py
+++ 8. Dataset & Dataloader.py
@@ -7,8 +7,8 @@ import math
 class WineData(Dataset):
     def __init__(self):
         data = np.loadtxt('wine.csv',delimiter = ',',dtype = np.float32,skiprows = 1)
-        self.x = data[:,1:]
-        self.y = data[:,[0]]
+        self.x = torch.from_numpy(data[:,1:])
+        self.y = torch.from_numpy(data[:,[0]])
         self.n_samples = data.shape[0]
 
     def __getitem__(self, index):
@@ -16,7 +16,7 @@ class WineData(Dataset):
     
     def __len__(self):
         return self.n_samples
-    
+
 dataset = WineData()
 print(dataset[5])
 
--- 9. Dataset Transform.py
+++ 9. Dataset Transform.py
@@ -13,19 +13,56 @@ class WineData(Dataset):
         self.x = data[:,1:]
         self.y = data[:,[0]]
         self.n_samples = data.shape[0]
+        self.transform = transforms # we apply the transform here if it is not None
 
     def __getitem__(self, index):
-        return self.x[index] , self.y[index]
-    
+        sample = self.x[index] , self.y[index]
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+
     def __len__(self):
         return self.n_samples
-    
-dataset = WineData()
-print(dataset[5]) 
 
 
-# we can also use Dataloader
+# __call__ is a special method in python which is called when we call an object
+    # exmaple of __call__ method is given below
+    # class A:
+    #     def __call__(self):
+    #         print("call method is called")
+    # a = A()
+    # a() # this will call the __call__ method
 
-# dataloader = DataLoader(dataset=dataset ,batch_size = 4 , shuffle = True) # this is a dataloader object
-# data_iter = iter(dataloader)
-# print(next(data_iter))
+# simple class to convert numpy array to tensors
+class ConvertToTensor:
+    def __call__(self,sample): 
+        inputs , targets = sample
+        return torch.from_numpy(inputs) , torch.from_numpy(targets)
+
+class MulTransform: # this class will multiply the inputs by a factor
+    def __init__(self,factor):
+        self.factor = factor
+
+    def __call__(self,sample):
+        inputs , targets = sample
+        inputs *= self.factor
+        return inputs , targets
+
+
+# example of using the ConvertToTensor class
+# dataset = WineData(transforms = ConvertToTensor()) # we pass the transform object here
+# features,labels = dataset[0]
+# print(type(labels),type(features))
+
+# example of using the MulTransform class and ConvertToTensor class
+a = WineData()
+f,l = a[0]
+print('printing features before transformation',f)
+
+
+composed = torchvision.transforms.Compose([ConvertToTensor(),MulTransform(5)])
+d = WineData(transforms = composed)
+features,labels  = d[0]
+print(features)
+print(type(labels),type(features))
+
